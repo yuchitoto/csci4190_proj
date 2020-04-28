dc75dbf194921e39782796eafc613e9b555b8d3f
Yu Chi To <yuchitoto@gmail.com>
2020-04-29 02:35:47 +0800

    update

--- tester.py
+++ tester.py
@@ -6,27 +6,38 @@ from sirs import sirs
 from graph import *
 import matplotlib.pyplot as plt
 
-p = 0.7
-i = 5
-r = 3
+p = 0.1
+i = 3
+r = 1
 
-print("infect p: {}".format(p))
-print("infectious period: {}".format(i))
-print("removed period: {}".format(r))
-
-print()
-print("sir model")
-
-covid = sirs(p,i,r,graph)
-
-turns = 100
+turns = 10
 crit = 1
 initInfect = 3
 
-infect, removed = covid.start(turns, initInfect, crit)
+series = []
+while p <= 1:
+    print("infect p: {}".format(p))
+    print("infectious period: {}".format(i))
+    print("removed period: {}".format(r))
+
+    print()
+    print("sir model")
+
+    covid = sir(p,i,r,graph)
+
+    infect, removed = covid.start(turns, initInfect, crit)
+    series.append(infect)
+
+    '''rule = np.arange(0,turns+1,step=crit)
+
+    plt.plot(rule, infect, color='red', label='infected')
+    plt.plot(rule, removed, color='black', label='removed')
+    plt.show()'''
+    p += 0.1
 
 rule = np.arange(0,turns+1,step=crit)
+for index,i in enumerate(series,start=1):
+    plt.plot(rule,infect,label='x='+str(index))
 
-plt.plot(rule, infect, color='red', label='infected')
-plt.plot(rule, removed, color='black', label='removed')
+plt.legend()
 plt.show()
