447be42a4c6733884a19c32d1124b0710bf2ffab
Yu Chi To <yuchitoto@gmail.com>
2020-04-29 20:34:16 +0800

    update tester.py

--- tester.py
+++ tester.py
@@ -35,20 +35,20 @@ while p <= 1:
         ift.append(infect)
         rmd.append(removed)
 
-        ax[0].plot(rule,infect)
-        ax[1].plot(rule,removed)
+        ax[0].plot(rule,infect, linestyle='--', linewidth=0.5)
+        ax[1].plot(rule,removed, linestyle='--', linewidth=0.5)
 
     ift = np.array(ift)
     rmd = np.array(rmd)
-    ax[0].plot(rule,np.average(ift,axis=0))
-    ax[1].plot(rule,np.average(rmd,axis=0))
+    ax[0].plot(rule,np.average(ift,axis=0), linewidth=0.5)
+    ax[1].plot(rule,np.average(rmd,axis=0), linewidth=0.5)
     plt.show()
     series.append(np.average(ift,axis=0))
     grave.append(np.average(rmd,axis=0))
     p += 0.1
 
 for index,i in enumerate(series,start=1):
-    plt.plot(rule,i,label='p='+str(index/10))
+    plt.plot(rule,i,label='p='+str(index/10), linewidth=0.5)
     #print(i)
 
 plt.title("Number of infected in {} model".format(model))
@@ -56,7 +56,7 @@ plt.legend()
 plt.show()
 
 for index,i in enumerate(grave,start=1):
-    plt.plot(rule,i,label="p="+str(index/10))
+    plt.plot(rule,i,label="p="+str(index/10), linewidth=0.5)
 
 plt.title("Number of removed in {} model".format(model))
 plt.legend()
