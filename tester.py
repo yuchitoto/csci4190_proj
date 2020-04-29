b3e3f4cd9e7337ab3642cec80f70764eefdfa916
Yu Chi To <yuchitoto@gmail.com>
2020-04-29 21:55:28 +0800

    failed to pass back list of list, trying concat

--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ from sis import sis
 from sirs import sirs
 from graph import *
 import matplotlib.pyplot as plt
-from multiprocessing import Pool, Lock
+from multiprocessing import Pool, Lock, Manager
 
 p = 0.1
 i = 3
@@ -23,21 +23,17 @@ grave = []
 
 rule = np.arange(0,turns+1,step=crit)
 
-def wuhan(lift, rmd, ind):
+def wuhan(lft, rmd, ind):
     covid = sir(p,i,r,graph)
     print("Epoch: {}/{}".format(ind+1,size))
 
     infect, removed = covid.start(turns, initInfect, crit)
 
-    ax[0].plot(rule,infect)
-    ax[1].plot(rule,removed)
+    ax[0].plot(rule,infect, linestyle='--', linewidth=0.5)
+    ax[1].plot(rule,removed, linestyle='--', linewidth=0.5)
 
-    lock.acquire()
-    try:
-        ift.append(infect)
-        rmd.append(removed)
-    finally:
-        lock.release()
+    lft+infect
+    rmd+removed
 
 
 locker = Lock()
@@ -65,22 +61,33 @@ if __name__ == '__main__':
             ax[1].plot(rule,removed)'''
 
         with Pool(processes=10,initializer=init, initargs=(locker,)) as p:
-            for ind in range(size):
-                p.apply_async(wuhan, args=(ift,rmd,ind))
-            p.close()
-            p.join()
+            with Manager() as manager:
+                inft = manager.list()
+                rmvd = manager.list()
+                for ind in range(size):
+                    p.apply_async(wuhan, args=(inft, rmvd, ind))
+                p.close()
+                p.join()
+                ift = inft
+                rmd = rmvd
 
-        ift = np.array(ift)
-        rmd = np.array(rmd)
-        ax[0].plot(rule,np.average(ift,axis=0))
-        ax[1].plot(rule,np.average(rmd,axis=0))
-        plt.show()
-        series.append(np.average(ift,axis=0))
-        grave.append(np.average(rmd,axis=0))
-        p += 0.1
+                ift = np.array(ift).reshape((len(rule), size))
+                rmd = np.array(rmd).reshape((len(rule), size))
+                print(ift)
+                print(rmd)
+                ax[0].plot(rule,np.average(ift,axis=0), linestyle='-', linewidth=0.5, label='Avg')
+                ax[1].plot(rule,np.average(rmd,axis=0), linestyle='-', linewidth=0.5, label='Avg')
+                ax[0].legend()
+                ax[1].legend()
+                ax[0].title("Infected in p="+str(p))
+                ax[1].title("Removed in p="+str(p))
+                plt.show()
+                series.append(np.average(ift,axis=0))
+                grave.append(np.average(rmd,axis=0))
+                p += 0.1
 
     for index,i in enumerate(series,start=1):
-        plt.plot(rule,i,label='p='+str(index/10))
+        plt.plot(rule,i,label='p='+str(index/10), linewidth=0.5)
         #print(i)
 
     plt.title("Average number of infected in {} model of {} simulations".format(model,size))
@@ -88,7 +95,7 @@ if __name__ == '__main__':
     plt.show()
 
     for index,i in enumerate(grave,start=1):
-        plt.plot(rule,i,label="p="+str(index/10))
+        plt.plot(rule,i,label="p="+str(index/10), linewidth=0.5)
 
     plt.title("Average number of removed in {} model of {} simulations".format(model,size))
     plt.legend()
